9c01647bdd4013e764857a13d4778f638a6aaca5
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:49:45 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Se citesc doua numere de la tastatura, x si y.
+* Scrieti un program care va verifica daca x este divizil cu y
+* In cazul in care "x" este divizibil cu "y", programul va afisa "divizibil", iar in caz contrar, "indivizibil"
+*/
+
+Console.WriteLine("Introduceti valoarea pentru x:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentry y:");
+int y = int.Parse(Console.ReadLine());
+
+
+if (x % y == 0)
+{
+    Console.WriteLine("divizil");
+}
+else
+{
+    Console.WriteLine("indivizil");
+}
